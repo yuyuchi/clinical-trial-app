90713300527ed322587c5786f38aed8c0f79b92c
Yuyu <yuchi@xfers.com>
2023-03-02 12:08:45 +0800

    feat: 🎸 Setup test

---
+++ src/setupTests.ts
@@ -0,0 +1,19 @@
+import server from './mocks/server';
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
